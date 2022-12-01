49244cc06c6831cf85d557023bbc3d3033e70ac5
Ankita Sahoo <2ankitasahoo13@gmail.com>
2022-11-30 23:36:41 -0500

    marker for start and end added

--- src/client/src/Components/Map.js
+++ src/client/src/Components/Map.js
@@ -1,10 +1,10 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
-import { Container } from "../Store/Provider"
-
+import { Container } from "../Store/Provider";
+import { useState, useEffect } from "react";
 
 let DefaultIcon = L.icon({
   iconUrl: icon,
@@ -14,21 +14,49 @@ let DefaultIcon = L.icon({
 L.Marker.prototype.options.icon = DefaultIcon;
 
 export const Map = () => {
-
+  const [zoomLocation, setZoomLocation] = useState([42.3754, -72.5193]);
+  const [markers, setMarkers] = useState({ startMarker: [], endMarker: [] });
   const container = Container.useContainer();
-  console.log(container.startCoordinate, container.endCoordinate);
+
+  useEffect(() => {
+    setZoomLocation(container.newLocation);
+    console.log(markers);
+    let newMarker = markers;
+    newMarker.startMarker = container.startCoordinate;
+    newMarker.endMarker = container.endCoordinate;
+    setMarkers(newMarker);
+  }, [
+    container.newLocation,
+    container.startCoordinate,
+    container.endCoordinate,
+  ]);
 
   return (
     <MapContainer
-      center={[42.3754, -72.5193]}
-      zoom={13}
+      center={zoomLocation}
+      zoom={10}
       scrollWheelZoom={false}
       className="mainMap"
+      key={`${zoomLocation[0]}-${zoomLocation[1]}`}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {markers.startMarker.length == 0 ? (
+        <></>
+      ) : (
+        <Marker
+          position={markers.startMarker}
+        />
+      )}
+      {markers.endMarker.length == 0 ? (
+        <></>
+      ) : (
+        <Marker
+          position={markers.endMarker}
+        />
+      )}
     </MapContainer>
   );
 };
--- src/client/src/Store/Provider.js
+++ src/client/src/Store/Provider.js
@@ -1,18 +1,25 @@
 import { createContainer } from "unstated-next"
 import { useState } from "react"
 
-function useCounter(initialState = {startResults: "", endResults: ""}) {
+function useCounter(initialState = {startResults: [42.3754, -72.5193], endResults: [42.3754, -72.5193], newLocation: [42.3754, -72.5193]}) {
 
   let [startCoordinate, setStartCoordinate] = useState(initialState.startResults)
   let [endCoordinate, setEndCoordinate] = useState(initialState.endResults)
+  let [newLocation, setNewLocation] = useState(initialState.newLocation)
 
   let callStart = (value) => {
-    setStartCoordinate(value)
+    let res = value.split(",").map(Number);
+    res.reverse();
+    setStartCoordinate(res);
+    setNewLocation(res);
   }
   let callEnd = (value) => {
-    setEndCoordinate(value);
+    let res = value.split(",").map(Number);
+    res.reverse();
+    setEndCoordinate(res);
+    setNewLocation(res);
   }
-  return { startCoordinate, endCoordinate, callStart, callEnd }
+  return { startCoordinate, endCoordinate, newLocation, callStart, callEnd }
 }
 
 export const Container = createContainer(useCounter)
