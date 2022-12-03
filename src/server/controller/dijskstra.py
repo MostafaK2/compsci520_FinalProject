03778f63d861b405c730ae233d2cdd52e319dbd0
nyiell <116977151+nyiell@users.noreply.github.com>
2022-12-03 14:37:57 -0500

    Update dijkstra

--- src/server/controller/dijskstra.py
+++ src/server/controller/dijskstra.py
@@ -14,7 +14,7 @@ class Pair(object):
 G = ox.graph_from_place('Sutherland Shire Council', network_type='drive')
 
 print("Shortest Paths Using In-Built Libraries")
-print(next(ox.k_shortest_paths(G, 1839271812, 668727077, 5, weight = 'length')))
+print(next(ox.k_shortest_paths(G, 1839271812, 668727077, 1, weight = 'length')))
 
 def dijkstra(G, src, dest):
     visited = set()
@@ -25,14 +25,13 @@ def dijkstra(G, src, dest):
         object = heapq.heappop(pq)
         print("path print check ",object.path)
         if object.node == dest:
-        print("found destination")
-        print("path", object.path)
-        break
+            print("found destination")
+            print("path", object.path)
+            break
         if object.visited == True:
-        continue
+            continue
         object.visited = True
         for src, next, length in G.edges(object.node, data=True):
-            print(length['length'])
             newObject = Pair(next, length['length'] + object.weight, False, object.path + " " + str(next))
             heapq.heappush(pq, newObject)
 
