631fb80e15812f6eed8d3074f05376b852805b91
Beena Kumari <beenakumari@vl965-172-31-124-96.wireless.umass.edu>
2022-12-07 15:47:02 -0500

    updated utility file

--- src/server/utility.py
+++ src/server/utility.py
@@ -2,6 +2,9 @@ import osmnx as ox
 
 def save_graph(place, filename):
     G = ox.graph_from_place(place, network_type="drive")
+    api_key = 'AIzaSyBZtVsQkxLlEp63rHfRCA1vXWSEverMKSs'
+    G = ox.elevation.add_node_elevations(G, api_key, precision=3)
+    G = ox.elevation.add_edge_grades(G, add_absolute=True, precision=3)
     ox.save_graphml(G, filename)
 
 if __name__ == "__main__":
