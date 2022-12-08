912b8388b4ec3d835abe85f25a0ec5b1c5db17e8
nyiell <116977151+nyiell@users.noreply.github.com>
2022-12-07 20:19:40 -0500

    Corrected DFS and extracted length

--- src/server/controller/dfs.py
+++ src/server/controller/dfs.py
@@ -1,17 +1,25 @@
 import osmnx as ox
 import sys
 
-def dfs(G, src, dest, visited):
+class Node(object):
+    def __init__(self, node, weight, path):
+        self.node = node
+        self.weight = weight
+        self.path = path
+   
+G = ox.graph_from_place('Sutherland Shire Council', network_type='drive')
+def dfs(G, src, dest, visited, length):
     if src == dest:
-        return 0
+        return length
     visited.append(src)
     min_dis = float('inf')
-    for src, nxt, dic in G.edges(1839271812, data=True):
+    for src, nxt, dic in G.edges(src, data=True):
         if not nxt in visited:
-            dis = dfs(G, nxt, dest, visited)
-            print(dis)
+            dis = dfs(G, nxt, dest, visited, dic['length'] + length)
             if dis < float('inf'):
-                min_dis = min(min_dis, dis + dic[length])
-
-    if src in visited: visited.remove(src)
+                min_dis = min(min_dis, dis + dic['length'])
     return min_dis
+
+
+path_len = dfs(G, 1839271812, 668727077,[], 0)
+print(path_len)
