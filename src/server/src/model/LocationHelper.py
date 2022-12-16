7cc7fd22d28388af9bb1dd5d22fc31ed97b6b1e4
Ankita Sahoo <2ankitasahoo13@gmail.com>
2022-12-16 14:39:59 -0500

    model view controller refactoring

---
+++ src/server/src/model/LocationHelper.py
@@ -0,0 +1,25 @@
+from geopy.geocoders import Nominatim
+
+def get_details(src_point, dest_point):
+    geolocator = Nominatim(user_agent="EleNa-server")
+    location_src = geolocator.reverse(str(src_point[1]) + ", " + str(src_point[0]))
+    location_dest = geolocator.reverse(str(dest_point[1]) + ", " + str(dest_point[0]))
+    src_country = location_src.raw['address']['country']
+    dest_country = location_dest.raw['address']['country']
+    src_state = location_src.raw['address']['state']
+    dest_state = location_dest.raw['address']['state']
+    if ("town" in location_src.raw['address'].keys()):
+        src_city = location_src.raw['address']['town']
+    else:
+        src_city = location_src.raw['address']['city']
+
+    if ("town" in location_dest.raw['address'].keys()):
+        dest_city = location_dest.raw['address']['town']
+    else:
+        dest_city = location_dest.raw['address']['city']
+    print(src_city, dest_city, src_state, dest_state, src_country, dest_country)
+
+    if (src_country != dest_country or src_state != dest_state or src_city != dest_city):
+        return "", "", ""
+    else:
+        return src_city, src_state, src_country
--- src/server/test/test.py
+++ src/server/test/test.py
@@ -1,9 +1,6 @@
 import unittest
-from controller.bfs import perform_bfs
-from controller.dfs import dfs
-from controller.dijkstraElev import dijkstra_elev
-from controller.dijkstra import dijkstra
-from controller.astar import astar
+from src.controller.dijkstra import dijkstra
+from src.controller.astar import astar
 import osmnx as ox
 
 class test_backend(unittest.TestCase):
@@ -12,39 +9,21 @@ class test_backend(unittest.TestCase):
     G = ox.elevation.add_node_elevations_google(G, api_key, precision=3)
     G = ox.elevation.add_edge_grades(G, add_absolute=True, precision=3)
 
-    def test_bfs(self):
-        src_node = 66711889
-        dest_node = 66593243
-        dis = perform_bfs(G, src_node, dest_node)
-        dis = ' '.join(map(str, dis))
-        expected = ox.shortest_path(G, src_node, dest_node, weight="length")
-        expected = ' '.join(map(str, expected))
-        self.assertEqual(dis, expected)
-
-    def test_dfs(self):
-        src_node = 66711889
-        dest_node = 66593243
-        dis = dfs(G, src_node, dest_node)
-        dis = ' '.join(map(str, dis))
-        expected = ox.shortest_path(G, src_node, dest_node, weight="length")
-        expected = ' '.join(map(str, expected))
-        self.assertEqual(dis, expected)
-
     def test_astar(self):
         src_node = 66711889
         dest_node = 66593243
-        dis = astar(G, src_node, dest_node)
+        dis = astar(self.G, src_node, dest_node)
         dis = ' '.join(map(str, dis))
-        expected = ox.shortest_path(G, src_node, dest_node, weight="length")
+        expected = ox.shortest_path(self.G, src_node, dest_node, weight="length")
         expected = ' '.join(map(str, expected))
         self.assertEqual(dis, expected)
 
     def test_dijkstra(self):
         src_node = 66711889
         dest_node = 66593243
-        dis = dijkstra(G, src_node, dest_node)
+        dis = dijkstra(self.G, src_node, dest_node)
         dis = ' '.join(map(str, dis))
-        expected = ox.shortest_path(G, src_node, dest_node, weight="length")
+        expected = ox.shortest_path(self.G, src_node, dest_node, weight="length")
         expected = ' '.join(map(str, expected))
         self.assertEqual(dis, expected)
 
